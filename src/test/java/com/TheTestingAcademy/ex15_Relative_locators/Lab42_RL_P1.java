90b99dd464567e56eb2730605b58513a5d03c665
Reena Singh <reenakumari8261@gmail.com>
2025-05-31 08:20:23 +0530

    Selenium Automation Testing

---
+++ src/test/java/com/TheTestingAcademy/ex15_Relative_locators/Lab42_RL_P1.java
@@ -0,0 +1,36 @@
+package com.TheTestingAcademy.ex15_Relative_locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.*;
+
+import com.TheTestingAcademy.CommonToAll;
+
+public class Lab42_RL_P1 extends CommonToAll {
+
+    @Test
+    public void test_js() {
+        driver.get("https://awesomeqa.com/practice.html");
+        driver.manage().window().maximize();
+
+
+        WebElement span_element = driver.findElement(By.xpath("//span[text()=\"Years of Experience\"]"));
+//        driver.findElement((with(By.id("exp-1")).toRightOf(span_element))).click();
+        driver.findElement((with(By.id("exp-2")).toRightOf(span_element))).click();
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/test/java/com/TheTestingAcademy/ex17_DataDrivenTesting_POI/Lab49_DDT_TestNG_POI_REAL.java
@@ -0,0 +1,25 @@
+package com.TheTestingAcademy.ex17_DataDrivenTesting_POI;
+
+import com.TheTestingAcademy.CommonToAll;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Lab49_DDT_TestNG_POI_REAL extends CommonToAll {
+
+    @Test(dataProvider = "getData")
+    public void test_vwo_login(String email,String password) {
+        System.out.println(email + " - "+ password);
+
+
+    }
+
+    @DataProvider
+    public Object[][] getData(){
+        // READ THE DATA FROM THE EXCEL FILE
+        // GIVE THEM IN THE 2D ARRAY
+        return UtilExcel.getTestDataFromExcel("sheet1");
+
+    }
+
+
+}
